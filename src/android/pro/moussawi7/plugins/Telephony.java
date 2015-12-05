5f04b66187322c864c2844f3e9416c9f340f6835
aSedali <toseyyedali@gmail.com>
2015-12-05 14:45:46 +0330

    Update Telephony.java

--- src/android/pro/moussawi7/plugins/Telephony.java
+++ src/android/pro/moussawi7/plugins/Telephony.java
@@ -7,25 +7,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.content.Context;
 import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 
 public class Telephony extends CordovaPlugin {
 	
 @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {	
     try {
-    	Context context=this.cordova.getActivity();
-    	    TelephonyManager mManager =(TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
+TelephonyManager Tel = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+    	    GsmCellLocation mManager =(GsmCellLocation) Tel.getCellLocation();
 
-      	    String NetworkOperator =   mManager.getNetworkOperator();
-    	    String PhoneNumber =   mManager.getLine1Number();
-    	    String SimSerialNumber= mManager.getSimSerialNumber();
-    	    String NetworkCountry=mManager.getNetworkCountryIso();
+      	    String NetworkOperator =   mManager.getCellID();
+    	    String PhoneNumber =   mManager.getCellID();
+    	    String SimSerialNumber= mManager.getLac();
+    	    String NetworkCountry=mManager.getLac();
     	    
             JSONObject result = new JSONObject();
-    	    result.put("NetworkOperator",NetworkOperator);
-    	    result.put("PhoneNumber",PhoneNumber);
-    	    result.put("SimSerialNumber",SimSerialNumber);
-    	    result.put("NetworkCountry",NetworkCountry);
+    	    result.put("cell",NetworkOperator);
+    	    result.put("cell2",PhoneNumber);
+    	    result.put("lac",SimSerialNumber);
+    	    result.put("lac2",NetworkCountry);
     	    
             callbackContext.success(result);
        return true;
