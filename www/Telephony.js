2fc847377a9d55431f2b308aa72c47bd8cb566f4
aSedali <toseyyedali@gmail.com>
2015-12-09 08:52:48 +0330

    Update Telephony.js

--- www/Telephony.js
+++ www/Telephony.js
@@ -9,12 +9,8 @@ Telephony.install = function () {
   if (!window.plugins) {
     window.plugins = {};
   }
-  console.log("new request");
-  console.log(window.plugins);
-    window.plugins.Telephony = new Telephony();
-      console.log(window.plugins);
-//	setInterval(function(){   window.plugins.Telephony = new Telephony(); }, 6000);
-   return window.plugins.Telephony;
 
+  window.plugins.Telephony = new Telephony();
+  return window.plugins.Telephony;
 };
 cordova.addConstructor(Telephony.install);
